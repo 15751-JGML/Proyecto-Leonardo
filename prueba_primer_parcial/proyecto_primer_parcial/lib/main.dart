512b6bb1246626520226838c71695699a14b5fe0
Jennifer Morales <201811078@tese.edu.mx>
2021-05-03 13:41:34 -0500

    Proyecto JSON

---
+++ prueba_primer_parcial/proyecto_primer_parcial/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_primer_parcial/icono_string_util.dart';
+import 'package:proyecto_primer_parcial/providers_menu.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Proyecto Departamental',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Proyecto"),
+      ),
+      body: _lista(context),
+    );
+  }
+
+  Widget _lista(BuildContext context) {
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(children: _listItems(snapshot.data, context));
+      },
+    );
+  }
+
+  List<Widget> _listItems(List<dynamic> data, BuildContext context) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right_outlined),
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) =>
+                _buildPopUpDialog(context, opt['texto']),
+          );
+        },
+      );
+      opciones..add(widgetTemp)..add(Divider());
+    });
+    return opciones;
+  }
+
+  Widget _buildPopUpDialog(BuildContext context, String opt) {
+    return new AlertDialog(
+      title: const Text('Diste click en la opcion '),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Hola soy la opcion :' + opt),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColorDark,
+          child: const Text('cerrar'),
+        ),
+      ],
+    );
+  }
+}
